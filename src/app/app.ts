38afa2273396e1f25657c3553b83455a718b2f75
Csaba Valyi <csaba.valyi1@gmail.com>
2015-11-18 18:14:53 +0100

    multiple heroes displayed on page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,26 @@
-import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
 
 class Hero {
 	id: number;
 	name: string;
 }
 
+var HEROES: Hero[] = [
+	{ "id": 11, "name": "Mr. Nice" },
+	{ "id": 12, "name": "Narco" },
+	{ "id": 13, "name": "Bombasto" },
+	{ "id": 14, "name": "Celeritas" },
+	{ "id": 15, "name": "Magneta" },
+	{ "id": 16, "name": "RubberMan" },
+	{ "id": 17, "name": "Dynama" },
+	{ "id": 18, "name": "Dr IQ" },
+	{ "id": 19, "name": "Magma" },
+	{ "id": 20, "name": "Tornado" }
+];
+
 @Component({
 	selector: 'my-app',
-	directives: [FORM_DIRECTIVES],
+	directives: [FORM_DIRECTIVES, NgFor],
 	template: `
 		<h1>{{title}}</h1>
 		<div><label>id: </label>{{hero.id}}</div>
@@ -15,7 +28,13 @@ class Hero {
 			<label>name: </label>{{hero.name}}
 			<div><input [(ng-model)]="hero.name" placeholder="name"></div>
 		</div>
-		`
+		<h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ng-for="#hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+        </ul>
+		`,
 })
 class AppComponent {
 	public title = 'Tour of Heroes';
@@ -23,6 +42,7 @@ class AppComponent {
 		id: 1,
 		name: 'Windstorm'
 	};
+    public heroes = HEROES;
 }
 
 bootstrap(AppComponent);
